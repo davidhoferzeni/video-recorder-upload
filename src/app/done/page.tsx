c87b99b73d4da21e80767246ae828ef5ba6d888f
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-25 13:51:50 +0200

    Added function to return home

--- src/app/done/page.tsx
+++ src/app/done/page.tsx
@@ -1,11 +1,18 @@
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+'use client';
+import { useRouter } from 'next/navigation';
 
 export default function Done() {
+
+    const router = useRouter();
+
+    function returnHome() {
+        router.push('/');
+      }
+
     return (
     <div className='flex min-h-screen flex-col items-center justify-center p-4 gap-64'>
-    <div>Danke für dein Video</div>
-    <button className='btn btn-blue'>Noch eines aufnehmen?</button>
+    <h1 className='text-3xl'>Danke für dein Video!</h1>
+    <button className='btn btn-blue' onClick={returnHome}>Noch eines aufnehmen?</button>
     </div>
     );
 }
