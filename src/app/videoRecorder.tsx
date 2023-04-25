0c8fc41bd8b1cef0a96c96b4c6c868e87590ae1d
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-25 13:42:48 +0200

    Added video recorder client component

---
+++ src/app/videoRecorder.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import ReactVideoRecorder, { ReactVideoRecorderProps } from 'react-video-recorder';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+const VideoRecorderClient = () => {
+  const [currentVideo, setCurrentVideo] = React.useState<Blob | null>(null);
+
+  const router = useRouter();
+
+  function resetVideo() {
+    console.log('resetting video');
+    setCurrentVideo(null);
+  }
+
+  function handleClick() {
+    console.log('videoBlob', currentVideo);
+    if (!currentVideo) {
+      console.warn('No video found!');
+    }
+    const formData = new FormData();
+    const timeStamp = new Date().toISOString().replace(/[-:T]/g, '').replace(/\.\d\d\dZ/, '');
+    formData.append('fileType', 'mp4');
+    formData.append('fileName', `${timeStamp}_Video`);
+    formData.append('fileToUpload', currentVideo!);
+    formData.append('submit', 'Upload Video');
+    const response = fetch('https://sandbox.luvdav.com/File_Upload/upload.php', {
+      method: 'POST',
+      mode: 'no-cors',
+      body: formData
+    })
+    console.log(response);
+    router.push('/done')
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 gap-4">
+      <ReactVideoRecorder
+        wrapperClassName={'flex-auto h-64 btn-low'}
+        onRecordingComplete={(videoBlob: Blob) => {
+          setCurrentVideo(videoBlob);
+        }}
+        onStopReplaying={resetVideo}
+      />
+      {currentVideo &&
+        <button
+          onClick={handleClick}
+          className='btn btn-blue flex-initial h-12'
+        >
+          Senden!
+        </button>
+      }
+    </main>
+  );
+}
+
+export default VideoRecorderClient;
