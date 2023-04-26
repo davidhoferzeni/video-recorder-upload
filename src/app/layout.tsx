145604b9198369754031bf0bee73037afdb14431
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-26 06:52:42 +0200

    Changed font to Mulish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Mulish } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const mulish = Mulish({ subsets: ['latin'], weight: '300' })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mulish.className}>{children}</body>
     </html>
   )
 }
