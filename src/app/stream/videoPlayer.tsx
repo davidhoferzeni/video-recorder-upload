738ec4af1d725ab2952d10077f1eb0848f0844b1
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-28 10:00:10 +0200

    Added intermediate stream components

---
+++ src/app/stream/videoPlayer.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import VideoStreamClient from "./videoStream";
+import { ChallengeData } from "@/const/challenges";
+
+export default function VideoPlayerClient({ videos }: { videos: ChallengeData[] }) {
+    const [currentVideo, setCurrentVideo] = useState(videos[0]);
+    function nextVideo() {
+        if (videos.length === 1) {
+            return;
+        }
+        const currentIndex = videos.indexOf(currentVideo);
+        const nextVideoIndex = currentIndex < videos.length-1 && currentIndex >= 0 ? currentIndex+1 : 0;
+        setCurrentVideo(videos[nextVideoIndex]);
+    }
+    if (videos.length <= 0) {
+        return (
+            <div className='flex min-h-screen flex-col items-center justify-center p-8 gap-4'>
+                <h1 className='text-6xl'>Keine Videos verfügbar!</h1>
+            </div>
+        );
+    }
+    return (
+        <div className='flex min-h-screen flex-col items-center justify-center p-8 gap-4'>
+           <VideoStreamClient
+                    videoSource={currentVideo.url}
+                    onVideoEnd={nextVideo}
+                    loop={videos.length === 1}
+                >
+                </VideoStreamClient>
+            {/* <h1 className='text-6xl'>{currentVideo.label}</h1> */}
+            {/* <h1 className='text-6xl'>{currentVideo.dateRecorded.toLocaleString()}</h1> */}
+        </div>
+    );
+}
