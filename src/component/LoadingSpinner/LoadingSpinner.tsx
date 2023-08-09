9ba00fe36d027ec30d93e272fed6b83d041d9bc5
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-08-09 11:51:07 +0200

    Fixed compilation issue

--- src/component/LoadingSpinner/LoadingSpinner.tsx
+++ src/component/LoadingSpinner/LoadingSpinner.tsx
@@ -4,8 +4,8 @@ const LoadingSpinner = ({
   size = 3,
   color = "#c78665",
 }: {
-  size: number;
-  color: string;
+  size?: number;
+  color?: string;
 }) => {
   const styling = `w-${size *4} h-${size*4} border-solid border-[#f3f3f3] border-t-[${color}] border-t-solid border-${size*4} border-t-${size*4}`;
   return <div className={`${styles.loader}`}></div>;
