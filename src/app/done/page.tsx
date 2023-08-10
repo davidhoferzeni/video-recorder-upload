807bf31f961935158f9222a4937867a8ea4975e7
David Hofer-Zeni <david.hofer-zeni@bcc.oebb.at>
2023-08-10 10:29:29 +0200

    Removed retry button

--- src/app/done/page.tsx
+++ src/app/done/page.tsx
@@ -15,7 +15,7 @@ export default function Done() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center p-4 gap-64'>
       <h1 className='text-3xl'>Danke für dein Video!</h1>
-      <button className='btn btn-blue' onClick={returnHome}>Noch eines aufnehmen?</button>
+      {/* <button className='btn btn-blue' onClick={returnHome}>Noch eines aufnehmen?</button> */}
       {/* {currentChallenge &&
         <p className='text-6xl'>
           {currentChallenge.label}
