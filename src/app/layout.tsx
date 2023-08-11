032b5b1839df94ae2c657262642bae2f78bb7695
David Hofer-Zeni <david.hofer-zeni@bcc.oebb.at>
2023-08-11 09:01:34 +0200

    Fixed image component issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={mulish.className}>
-        <Image src="./logo.png" className="absolute w-24 h-24 bottom-0 right-0 z-10" alt="Logo" />
-        <Image src="./flowers.png" className="absolute w-72 w-72 -bottom-4 -left-24 z-10 rotate-180" alt="Flowers" />
-        <Image src="./flowers.png" className="absolute w-72 w-72 -top-4 -right-24 z-10" alt="Flowers" />
+        <div className="absolute w-24 h-24 bottom-0 right-0 z-10">
+          <Image fill={true} src="/logo.png" alt="Logo" />
+        </div>
+        <div className="absolute w-72 h-72 -bottom-4 -left-24 z-10 rotate-180">
+          <Image fill={true} src="/flowers.png" alt="Flowers" />
+        </div>
+        <div className="absolute w-72 h-72 -top-4 -right-24 z-10">
+          <Image fill={true} src="/flowers.png" alt="Flowers" />
+        </div>
         {children}
       </body>
     </html>
